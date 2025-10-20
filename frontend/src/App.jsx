e88b0e24f150708c5d989d8383d4a7b8c0d10df7
gcfreelancing <gabrielgcosta.trabalho@gmail.com>
2025-10-20 02:54:06 +0100

    chore: update root application files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,41 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/Layout'
+import ProtectedRoute from './components/ProtectedRoute'
 import Dashboard from './pages/Dashboard'
 import Timer from './pages/Timer'
 import Tasks from './pages/Tasks'
 import Settings from './pages/Settings'
-import React from 'react'
+import Login from './pages/Login'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 function App() {
     return (
-        <Router>
-            <Layout>
+        <AuthProvider>
+            <Router>
                 <Routes>
-                    <Route path="/" element={<Dashboard />} />
-                    <Route path="/timer" element={<Timer />} />
-                    <Route path="/tasks" element={<Tasks />} />
-                    <Route path="/settings" element={<Settings />} />
+                    {/* Login Route - Public */}
+                    <Route path="/login" element={<Login />} />
+
+                    {/* Protected Routes */}
+                    <Route
+                        path="/*"
+                        element={
+                            <ProtectedRoute>
+                                <Layout>
+                                    <Routes>
+                                        <Route path="/" element={<Dashboard />} />
+                                        <Route path="/timer" element={<Timer />} />
+                                        <Route path="/tasks" element={<Tasks />} />
+                                        <Route path="/settings" element={<Settings />} />
+                                        <Route path="*" element={<Navigate to="/" replace />} />
+                                    </Routes>
+                                </Layout>
+                            </ProtectedRoute>
+                        }
+                    />
                 </Routes>
-            </Layout>
-        </Router>
+            </Router>
+        </AuthProvider>
     )
 }
 
